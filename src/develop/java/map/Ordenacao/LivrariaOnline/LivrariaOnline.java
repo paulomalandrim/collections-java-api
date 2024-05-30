09c412440e1afac243650fbf64d245ccfb60fb48
paulomalandrim <paulo.malandrim@gmail.com>
2024-05-30 00:10:06 -0300

    .

--- src/develop/java/map/Ordenacao/LivrariaOnline/LivrariaOnline.java
+++ src/develop/java/map/Ordenacao/LivrariaOnline/LivrariaOnline.java
@@ -1,7 +1,6 @@
 package develop.java.map.Ordenacao.LivrariaOnline;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class LivrariaOnline {
 
@@ -11,14 +10,49 @@ public class LivrariaOnline {
         this.livroMap = new HashMap<>();
     }
 
-    public void adicionarLivro(String isbn, Livro livro){
+    public void adicionarLivro(String link, Livro livro){
         livroMap.put(isbn, livro);
     }
 
     public void removerLivro(String titulo){
-        livroMap.
+        for (Map.Entry<String, Livro> livroEntry : livroMap.entrySet()){
+            if (livroEntry.getValue().getTitulo().equalsIgnoreCase(titulo)){
+                livroMap.remove(livroEntry.getKey());
+                break;
+            }
+        }
     }
 
+    public void exibirLivrosOrdenadosPorPreco(){
+        List<Map.Entry<String, Livro>> livroOrdenadoPorPreco = new ArrayList<>(livroMap.entrySet());
+        Collections.sort(livroOrdenadoPorPreco, new ComparatorPorPreco());
 
+        Map<String, Livro> livrosOrdenadosMap = new LinkedHashMap<>();
 
+        for (Map.Entry<String, Livro> entry : livroOrdenadoPorPreco){
+            livrosOrdenadosMap.put(entry.getKey(),entry.getValue());
+        }
+
+        System.out.println("Livros ordenados por preco:");
+
+    }
+
+    public
+
+    public static void main(String[] args) {
+        LivrariaOnline livrariaOnline = new LivrariaOnline();
+
+        livrariaOnline.adicionarLivro("https://x.gd/tK9eF", new Livro("Titulo 1", "Autor 1", 20.90D));
+        livrariaOnline.adicionarLivro("https://x.gd/FKHND", new Livro("Titulo 2", "Autor 2", 45.90D));
+        livrariaOnline.adicionarLivro("https://x.gd/4AQi1", new Livro("Titulo 3", "Autor 3", 17.90D));
+    }
+}
+
+class ComparatorPorPreco implements Comparator<Map.Entry<String, Livro>>{
+    @Override
+    public int compare(Map.Entry<String, Livro> o1, Map.Entry<String, Livro> o2) {
+        return Double.compare(
+                o1.getValue().getPreco(),
+                o2.getValue().getPreco());
+    }
 }
