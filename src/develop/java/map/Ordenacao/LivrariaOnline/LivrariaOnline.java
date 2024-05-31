7d8649fda24b99474005e936e614a46e91f82c4c
paulomalandrim <paulo.malandrim@gmail.com>
2024-05-31 14:18:26 -0300

    .

--- src/develop/java/map/Ordenacao/LivrariaOnline/LivrariaOnline.java
+++ src/develop/java/map/Ordenacao/LivrariaOnline/LivrariaOnline.java
@@ -11,7 +11,7 @@ public class LivrariaOnline {
     }
 
     public void adicionarLivro(String link, Livro livro){
-        livroMap.put(isbn, livro);
+        livroMap.put(link, livro);
     }
 
     public void removerLivro(String titulo){
@@ -33,18 +33,64 @@ public class LivrariaOnline {
             livrosOrdenadosMap.put(entry.getKey(),entry.getValue());
         }
 
-        System.out.println("Livros ordenados por preco:");
+        System.out.println("Livros ordenados por preco:" + livroOrdenadoPorPreco);
 
     }
 
-    public
+    public Map<String, Livro> pesquisarLivrosPorAutor(String autor){
+        List<Map.Entry<String, Livro>> listLivros = new ArrayList<>(livroMap.entrySet());
+        Map<String, Livro> livrosDoAutor = new HashMap<>();
+
+        for (Map.Entry<String, Livro> entry : listLivros){
+            if (entry.getValue().getAutor().equalsIgnoreCase(autor)){
+                livrosDoAutor.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return livrosDoAutor;
+    }
+
+    public Livro obterLivroMaisCaro(){
+        List<Map.Entry<String, Livro>> listLivros = new ArrayList<>(livroMap.entrySet());
+        Livro livroMaisCaro = null;
+
+        for (Map.Entry<String, Livro> entry : listLivros){
+            if (livroMaisCaro == null) livroMaisCaro = entry.getValue();
+            if (entry.getValue().getPreco() > livroMaisCaro.getPreco()){
+                livroMaisCaro = entry.getValue();
+            }
+        }
+
+        return livroMaisCaro;
+    }
+
+    public Livro obterLivroMaisBarato(){
+        List<Map.Entry<String, Livro>> listLivros = new ArrayList<>(livroMap.entrySet());
+        Livro livroMaisBarato = null;
+
+        for (Map.Entry<String, Livro> entry : listLivros) {
+            if (livroMaisBarato == null) livroMaisBarato = entry.getValue();
+            if (entry.getValue().getPreco() < livroMaisBarato.getPreco()){
+                livroMaisBarato = entry.getValue();
+            }
+        }
+
+        return livroMaisBarato;
+
+    }
 
     public static void main(String[] args) {
         LivrariaOnline livrariaOnline = new LivrariaOnline();
 
         livrariaOnline.adicionarLivro("https://x.gd/tK9eF", new Livro("Titulo 1", "Autor 1", 20.90D));
         livrariaOnline.adicionarLivro("https://x.gd/FKHND", new Livro("Titulo 2", "Autor 2", 45.90D));
+        livrariaOnline.adicionarLivro("https://x.gd/FKHN3", new Livro("Titulo 4", "Autor 2", 85.90D));
         livrariaOnline.adicionarLivro("https://x.gd/4AQi1", new Livro("Titulo 3", "Autor 3", 17.90D));
+
+        livrariaOnline.exibirLivrosOrdenadosPorPreco();
+
+        System.out.println("Livros do Autor 2: " + livrariaOnline.pesquisarLivrosPorAutor("Autor 2"));
+        System.out.println("O livro mais caro e: " + livrariaOnline.obterLivroMaisCaro());
+        System.out.println("O livro mais barato e: " + livrariaOnline.obterLivroMaisBarato());
     }
 }
 
